51b08702de0788da39b01cc0439015d89fa3ba8f
Darya Dybalina <dasha.dybalina1@gmail.com>
2024-05-30 01:13:03 +0300

    День недели

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -12,5 +12,25 @@ function showDate(){
     germanyOut.innerHTML = 'Дата и время для немецкой локали: ' + today.toLocaleString('de-AT');
     greekOut.innerHTML = 'Дата и время для греческой локали: ' + today.toLocaleString('el-GR')
     englishOut.innerHTML = 'Дата и время для американской локали: ' + today.toLocaleString('en-US')
+    
+    document.getElementById('year').innerHTML = 'Текущий год: ' + today.getFullYear();
+    const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    document.getElementById('month').innerHTML = 'Текущий месяц: ' + months[today.getMonth()];
+    document.getElementById('day').innerHTML = 'Текущий день: ' + today.getDate();
+    const weekDays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    document.getElementById('weekDay').innerHTML = 'Текущий день недели: ' + weekDays[today.getDay()];
+
+
+    document.getElementById('dayButton').addEventListener('click', function() {
+        YEAR = document.getElementById('YEAR').value; 
+        MONTH = document.getElementById('MONTH').value;
+        DAY = document.getElementById('DAY').value;
+    
+        const date = new Date(YEAR, MONTH - 1, DAY);
+        const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        const dayOfWeek = daysOfWeek[date.getDay()];
+    
+        document.getElementById('result').innerHTML = dayOfWeek;
+    })
 
 }
